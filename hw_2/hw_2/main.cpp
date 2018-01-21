92675091afb1db5deb8a79cf4f2dbce7d4346f50
Amelia Kaufman <amelia.kaufman@timeinc.com>
2018-01-21 14:43:52 -0500

    new hw repo

---
+++ hw_2/hw_2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hw_2
+//
+//  Created by Amelia Kaufman on 1/21/18.
+//  Copyright © 2018 Amelia Kaufman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
